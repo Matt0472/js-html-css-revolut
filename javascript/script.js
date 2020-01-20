3e1f71411f3a2f83fc33849a14ccf350892ad5cf
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-20 17:45:52 +0100

    esercizio quasi completo; mancano alcuni piccoli dettagli

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,14 @@
 $(document).ready(
   function() {
-    
+    $('.opening_link').mouseenter(
+      function() {
+      $(this).children('.dropdown').addClass('active');
+      }
+    );
+    $('.opening_link').mouseleave(
+      function() {
+      $(this).children('.dropdown').removeClass('active');
+      }
+    );
   }
 );
